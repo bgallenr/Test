710707c58d653b05f170c5b7d9eb8ff9f46ec630
bgallenr <bgallen@uw.edu>
2025-04-02 15:05:09 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package test2;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int result = add(a, b);
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
